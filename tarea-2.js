179e42be68bc1d685ea4a5b530d328d7b13f91ca
Ismabene <Ismabenedetti@hotmail.com>
2023-10-03 22:36:51 -0300

    Mejora como se muestran los datos
    Se agregan los datos en una variable para que aparezcan de manera mas prolija

--- tarea-2.js
+++ tarea-2.js
@@ -7,8 +7,9 @@ ingresar.onclick = function (){
   let primerNombre = document.querySelector("#primer-nombre").value ;
   let segundoNombre = document.querySelector("#segundo-nombre").value;
   let apellido = document.querySelector("#apellido").value;
-  let edad = document.querySelector("#edad-usuario").value
-  MostrarDatos.value = [primerNombre, segundoNombre, apellido,edad] ;
+  let edad = document.querySelector("#edad-usuario").value;
+  let datosCompletos = primerNombre + ", " + segundoNombre + ", " + apellido + ", " + edad + ".";  
+  MostrarDatos.value = datosCompletos
 
     titulo.textContent = "Bienvenido " + primerNombre
   return false;
